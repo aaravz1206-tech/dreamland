06bc490f5b2ab7552f93174fcd55d43e03baff20
IntellMeet Dev <dev@intellmeet.com>
2026-06-08 17:58:30 +0530

    Add script.js workaround for Render start command

---
+++ script.js
@@ -0,0 +1,3 @@
+// This file exists because Render's Start Command is currently hardcoded to 'node script.js'.
+// It simply imports the actual server file to start the application.
+require('./server.js');
